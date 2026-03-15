ea4742fc46417af5d94d7b42e6142cd3ca77c4b2
Orazio <22700499+orazioedoardo@users.noreply.github.com>
2026-03-15 21:14:38 +0100

    Add hashes for x86_64

--- build-linux-app.sh
+++ build-linux-app.sh
@@ -14,6 +14,15 @@ runtime_version="20251108"
 runtime_filename="runtime-$system_arch"
 runtime_baseurl="https://github.com/AppImage/type2-runtime/releases/download/$runtime_version"
 
+if [ "$system_arch" = "x86_64" ]; then
+	appimagetool_hash="ed4ce84f0d9caff66f50bcca6ff6f35aae54ce8135408b3fa33abfc3cb384eb0"
+	runtime_hash="2fca8b443c92510f1483a883f60061ad09b46b978b2631c807cd873a47ec260d"
+
+elif [ "$system_arch" = "aarch64" ]; then
+	appimagetool_hash="f0837e7448a0c1e4e650a93bb3e85802546e60654ef287576f46c71c126a9158"
+	runtime_hash="00cbdfcf917cc6c0ff6d3347d59e0ca1f7f45a6df1a428a0d6d8a78664d87444"
+fi
+
 # Shared libraries as of Ubuntu 22.04
 required_libs=(
 	libbsd.so.0
@@ -42,14 +51,15 @@ done
 if [ ! -f "$appimagetool_filename" ]; then
 	curl -sSf -L -O "$appimagetool_baseurl/$appimagetool_filename"
 	chmod +x "$appimagetool_filename"
-	echo "f0837e7448a0c1e4e650a93bb3e85802546e60654ef287576f46c71c126a9158  $appimagetool_filename" | shasum -a 256 -c
 fi
 
 if [ ! -f "$runtime_filename" ]; then
 	curl -sSf -L -O "$runtime_baseurl/$runtime_filename"
 	chmod +x "$runtime_filename"
-	echo "00cbdfcf917cc6c0ff6d3347d59e0ca1f7f45a6df1a428a0d6d8a78664d87444  $runtime_filename" | shasum -a 256 -c
 fi
+
+echo "$appimagetool_hash  $appimagetool_filename" | shasum -a 256 -c
+echo "$runtime_hash  $runtime_filename" | shasum -a 256 -c
 
 ./"$appimagetool_filename" --no-appstream --runtime-file "runtime-$system_arch" RecklessDrivin.AppDir "RecklessDrivin-$sw_version-linux-$system_arch.AppImage"
 
