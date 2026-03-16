91c0f8c2a47c8c08433e68120eeca5706cc84ede
Orazio <22700499+orazioedoardo@users.noreply.github.com>
2026-03-16 12:43:39 +0100

    Add macOS build script

---
+++ build-mac-app.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -xe
+
+mkdir -p build
+pushd build
+
+sdl_version='2.32.10'
+sdl_filename="SDL2-$sdl_version.dmg"
+sdl_url="https://github.com/libsdl-org/SDL/releases/download/release-$sdl_version/$sdl_filename"
+
+mount_point="$(mktemp -d)"
+
+if [ ! -f "$sdl_filename" ]; then
+	curl -sSf -L -O "$sdl_url"
+fi
+
+echo "4a7ac31640d70214e848f994be8a12849c0f97918a7e6c2e27a40036166d1a7f  $sdl_filename" | shasum -a 256 -c
+hdiutil attach "$sdl_filename" -mountpoint "$mount_point" -quiet
+cp -a "$mount_point/SDL2.framework" .
+hdiutil detach "$mount_point"
+
+popd
+cmake -S . -B build
+cmake --build build -j "$(nproc)"
+pushd build
+
+sw_version="1.0.2"
+
+sed -i '' "s/CHANGEME_SW_VERSION/$sw_version/" RecklessDrivin.app/Contents/Info.plist
+hdiutil create -fs HFS+ -srcfolder RecklessDrivin.app -volname "RecklessDrivin $sw_version" "RecklessDrivin-$sw_version-mac.dmg"
+rm -r RecklessDrivin.app
